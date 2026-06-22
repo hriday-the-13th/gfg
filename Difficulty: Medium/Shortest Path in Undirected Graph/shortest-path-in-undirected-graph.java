ee881480ecfb59038239b1d4114642faa38a9dff
Hriday Patgiri <51878066+hriday-the-13th@users.noreply.github.com>
2026-06-22 12:06:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Path in Undirected Graph/shortest-path-in-undirected-graph.java
+++ Difficulty: Medium/Shortest Path in Undirected Graph/shortest-path-in-undirected-graph.java
@@ -1,10 +1,8 @@
 class Solution {
-    public int[] shortestPath(int V, int[][] edges, int src) {
-        // code here
-        Queue<Integer> q = new LinkedList<>();
+    public record Pair(int distance, int node){}
         
-        int[] distance = new int[V];
-        Arrays.fill(distance, Integer.MAX_VALUE);
+    public int[] shortestPath(int V, int[][] edges, int src) {
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x, y) -> x.distance - y.distance);
         
         List<List<Integer>> adjList = new ArrayList<>();
         
@@ -20,36 +18,87 @@ class Solution {
             adjList.get(v).add(u);
         }
         
+        int[] distance = new int[V];
+        for(int i=0; i<V; i++){
+            distance[i] = (int) 1e9;
+        }
         
-        q.add(src);
+        pq.add(new Pair(0, src));
         distance[src] = 0;
         
-        bfs(q, adjList, distance);
+        while(!pq.isEmpty()){
+            Pair pair = pq.poll();
+            int dist = pair.distance();
+            int node = pair.node();
+            
+            for(int neighbour: adjList.get(node)){
+                if(dist + 1 < distance[neighbour]){
+                    distance[neighbour] = dist + 1;
+                    pq.add(new Pair(distance[neighbour], neighbour));
+                }
+            }
+        }
         
-        for (int i = 0; i < V; i++) {
-            if (distance[i] == Integer.MAX_VALUE) {
+        for(int i=0; i<V; i++){
+            if(distance[i] == (int) 1e9){
                 distance[i] = -1;
             }
         }
         
         return distance;
-        
-        
     }
     
-    public void bfs(Queue<Integer> q, List<List<Integer>> adjList, int[] distance){
+    // public int[] shortestPath(int V, int[][] edges, int src) {
+    //     // code here
+    //     Queue<Integer> q = new LinkedList<>();
         
-        while(!q.isEmpty()){
-            int node = q.poll();
-            int dist = distance[node];
+    //     int[] distance = new int[V];
+    //     Arrays.fill(distance, Integer.MAX_VALUE);
+        
+    //     List<List<Integer>> adjList = new ArrayList<>();
+        
+    //     for(int i=0; i<V; i++){
+    //         adjList.add(new ArrayList<>());
+    //     }
+        
+    //     for(int[] edge: edges){
+    //         int u = edge[0];
+    //         int v = edge[1];
             
-            for(int neighbour: adjList.get(node)){
-                if(distance[neighbour] == Integer.MAX_VALUE){
-                    q.add(neighbour);
-                    distance[neighbour] = dist + 1;
-                }
-            }
-        }
+    //         adjList.get(u).add(v);
+    //         adjList.get(v).add(u);
+    //     }
         
-    }
+        
+    //     q.add(src);
+    //     distance[src] = 0;
+        
+    //     bfs(q, adjList, distance);
+        
+    //     for (int i = 0; i < V; i++) {
+    //         if (distance[i] == Integer.MAX_VALUE) {
+    //             distance[i] = -1;
+    //         }
+    //     }
+        
+    //     return distance;
+        
+        
+    // }
+    
+    // public void bfs(Queue<Integer> q, List<List<Integer>> adjList, int[] distance){
+        
+    //     while(!q.isEmpty()){
+    //         int node = q.poll();
+    //         int dist = distance[node];
+            
+    //         for(int neighbour: adjList.get(node)){
+    //             if(distance[neighbour] == Integer.MAX_VALUE){
+    //                 q.add(neighbour);
+    //                 distance[neighbour] = dist + 1;
+    //             }
+    //         }
+    //     }
+        
+    // }
 }
